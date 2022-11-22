72e7f0e5ff4a73a54ab69f19d58d16294c5bd1b8
ctwrs <>
2022-11-22 13:22:34 +0100

    Fix crash on old webui versions

--- scripts/wildcard_recursive.py
+++ scripts/wildcard_recursive.py
@@ -453,7 +453,10 @@ class Script(scripts.Script):
 
         if debug: print(f'\nModel: {p.sampler_name}, Seed: {int(p.seed)}, Batch Count: {p.n_iter}, Batch Size: {p.batch_size}, CFG: {p.cfg_scale}, Steps: {p.steps}\nOriginal Prompt: "{p.prompt}"\nOriginal Negatives: "{p.negative_prompt}"\n')
         original_prompt = p.all_prompts[0]
-        original_negative = p.all_negative_prompts[0]
+        if hasattr(p, "all_negative_prompts"): # hasattr to fix crash on old webui versions
+            original_negative = p.all_negative_prompts[0]
+        else:
+            original_negative = ""
 
         TagLoader.files.clear()
         original_prompt = p.all_prompts[0]
@@ -486,7 +489,7 @@ class Script(scripts.Script):
                 if debug: print(f'Prompt: "{prompt}"')
 
                 negative = original_negative
-                if negative_prompt:
+                if negative_prompt and hasattr(p, "all_negative_prompts"): # hasattr to fix crash on old webui versions
                     negative += prompt_generator.get_negative_tags()
                     p.all_negative_prompts[index] = negative
                     if debug: print(f'Negative: "{negative}\n"')
