42cb859fbbbeabf4504456a745117620cbe1aa8b
ctwrs <>
2023-02-27 14:51:58 +0100

    Christian manga thumbnail

--- javascript/card_anim_on_hover.js
+++ javascript/card_anim_on_hover.js
@@ -5,12 +5,28 @@ window.onload = () => {
             clearInterval(interval);
             
             const cards = root.querySelectorAll('.card');
+            
+            let christianMangaJPG;
+
             for (let card of cards) {
                 const bgPNG = card.style.backgroundImage;
-                
+
+                if (!bgPNG &&card.getAttribute('onclick').includes('NSFW')) {
+                    console.log('NSFW',card.getAttribute('onclick'), card.getAttribute('onclick').includes('NSFW'))
+                    
+                    card.style.backgroundImage = christianMangaJPG;
+                }
 
                 if (bgPNG && bgPNG !== '' && bgPNG.includes('extensions')) {
                     const bgJPG = bgPNG.replace('.png', '.jpg');
+
+                    if (!christianMangaJPG) {
+                        const bgArr = bgJPG.split('/');
+                        const i = bgArr.indexOf('extensions');
+                        const endMtime = '&mtime' + bgJPG.split('&mtime')[1];
+                        christianMangaJPG = bgArr.slice(0, i + 2).join('/') + '/embeddings/Christian Manga for NSFW content.jpg' + endMtime;
+                    }
+
                     fetch(bgJPG.replace('url("', '').replace('")', '')).then(res => {
                         if (!res.ok) return;
                         card.style.backgroundImage = bgJPG;
